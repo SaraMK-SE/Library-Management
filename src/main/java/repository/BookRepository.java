96edd4e66b69e4fb7785ef1bacb2b55e707abbdd
Sara Alwabarneh <saramksw13@gmail.com>
2026-03-02 03:29:06 +0300

    added getAllBooks Method

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,8 +1,14 @@
 package repository;
 import dto.CreateBookDTO;
+import entity.Book;
 import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.Tuple;
 import io.vertx.core.Future;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookRepository {
 
   private final Pool pool;
@@ -11,8 +17,9 @@ public class BookRepository {
     this.pool = pool;
   }
 
+  //Create
   public Future<Void> addBook(CreateBookDTO book){
-  String sql = "insert into Books (title, author, isbn) values (@p1, @p2, @p3) ";
+  String sql = "insert into Books (title, author, isbn) values (@p1, @p2, @p3)";
 
   Tuple parameters = Tuple.of(book.getTitle(), book.getAuthor(), book.getIsbn() );
 
@@ -21,4 +28,30 @@ public class BookRepository {
              .mapEmpty();
 
   }
+  //Read
+  public Future<List<Book>> getAllBooks(){
+    String sql = "SELECT * FROM Books";
+
+    return pool.preparedQuery(sql)
+      .execute()
+      .map(rows -> {
+        List<Book> bookList = new ArrayList<>();
+
+        for (Row row : rows){
+          Book book = new Book();
+
+          book.setId(row.getInteger("id"));
+          book.setTitle(row.getString("title"));
+          book.setAuthor(row.getString("author"));
+          book.setIsbn(row.getString("isbn"));
+          book.setIsAvailable(row.getBoolean("is_Available"));
+          book.setCreatedAt(row.getLocalDateTime("created_at"));
+
+          bookList.add(book);
+        }
+
+        return bookList;
+      });
+  }
+
 }
